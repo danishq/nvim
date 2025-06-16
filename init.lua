2cac9f9e1ec88ee1f0fd054e7621de9d8a5f42f8
Danish Qureshi <dqureshi08@gmail.com>
2025-06-16 17:52:43 +0530

    updated inlayhint snacks

--- init.lua
+++ init.lua
@@ -2,4 +2,5 @@
 require("config.lazy")
 vim.keymap.set("n", "<leader>q", "<C-q>", { noremap = true, silent = true })
 vim.o.swapfile = false
-
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
